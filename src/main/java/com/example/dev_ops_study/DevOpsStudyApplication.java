08e1643754508d86bde8c55f796ba2014213955f
DESKTOP-2SHFLOJ\ngotr <ngotrung99@gmail.com>
2023-03-10 17:27:57 +0700

    Initial commit

---
+++ src/main/java/com/example/dev_ops_study/DevOpsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev_ops_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsStudyApplication.class, args);
+    }
+
+}
